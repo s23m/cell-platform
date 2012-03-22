2df1a266af91498cf1762fb65a33ab15a9909808
Jorn Bettin <Jorn.Bettin@s23m.org>
2012-03-23 10:46:44 +1100

    cell platform functional extensions

---
+++ org.s23m.cell.platform/src/main/java/org/s23m/cell/platform/models/Agency.java
@@ -0,0 +1,108 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is S23M.
+ *
+ * The Initial Developer of the Original Code is
+ * SoftMetaWare Ltd. (SoftMetaWare).
+ * Portions created by the Initial Developer are
+ * Copyright (C) 2011 SoftMetaWare Ltd.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ * Jorn Bettin
+ * ***** END LICENSE BLOCK ***** */
+package org.s23m.cell.platform.models;
+
+import static org.s23m.cell.S23MKernel.coreGraphs;
+
+import org.s23m.cell.Set;
+import org.s23m.cell.api.Instantiation;
+import org.s23m.cell.api.models.Root;
+import org.s23m.cell.api.models.S23MSemanticDomains;
+
+
+public class Agency {
+
+	private static final Set v0 = Instantiation.arrow(coreGraphs.visibility, Root.cellengineering, CellEngineering.agency);
+
+	public static final Set agent = CellEngineering.agency.addConcrete(coreGraphs.vertex, CellPlatformDomain.agent);
+	private static final Set s1 = Instantiation.arrow(coreGraphs.superSetReference, agent, coreGraphs.vertex);
+	public static final Set person = CellEngineering.agency.addConcrete(coreGraphs.vertex, CellPlatformDomain.person);
+	public static final Set system = CellEngineering.agency.addConcrete(coreGraphs.vertex, CellPlatformDomain.system);
+	private static final Set s2 = Instantiation.arrow(coreGraphs.superSetReference, person, agent);
+	private static final Set s3 = Instantiation.arrow(coreGraphs.superSetReference, system, agent);
+	//private static final Set v2 = Instantiation.arrow(coreGraphs.visibility, CellEngineering.agency, CellEngineering.timeConsciousness);
+	private static final Set v1 = Instantiation.arrow(coreGraphs.visibility, CellEngineering.agency, CellEngineering.language);
+	private static final Set v2 = Instantiation.arrow(coreGraphs.visibility, CellEngineering.agency, CellEngineering.jargon);
+
+
+	public static final Set perspective = Instantiation.arrow(coreGraphs.edge,
+			CellPlatformDomain.perspective,
+			S23MSemanticDomains.from,
+			agent,
+			S23MSemanticDomains.minCardinality_0,
+			S23MSemanticDomains.maxCardinality_n,
+			S23MSemanticDomains.isNavigable_FALSE,
+			S23MSemanticDomains.isContainer_FALSE,
+			S23MSemanticDomains.to,
+			agent,
+			S23MSemanticDomains.minCardinality_1,
+			S23MSemanticDomains.maxCardinality_1,
+			S23MSemanticDomains.isNavigable_TRUE,
+			S23MSemanticDomains.isContainer_FALSE
+	);
+	private static final Set s4 = Instantiation.arrow(coreGraphs.superSetReference, perspective, coreGraphs.edge);
+	public static final Set perspectiveV = perspective.addConcrete(coreGraphs.vertex, perspective);
+	private static final Set s5 = Instantiation.arrow(coreGraphs.superSetReference, perspectiveV, coreGraphs.vertex);
+
+	private static final Set v3 = Instantiation.arrow(coreGraphs.visibility, perspective, CellEngineering.language);
+	private static final Set v4 = Instantiation.arrow(coreGraphs.visibility, perspective, CellEngineering.jargon);
+
+	public static final Set perspective_to_jargon = Instantiation.arrow(coreGraphs.edge,
+			CellPlatformDomain.perspective_to_jargon,
+			perspectiveV,
+			perspectiveV,
+			S23MSemanticDomains.minCardinality_0,
+			S23MSemanticDomains.maxCardinality_n,
+			S23MSemanticDomains.isNavigable_FALSE,
+			S23MSemanticDomains.isContainer_FALSE,
+			CellPlatformDomain.jargon,
+			CellEngineering.language,
+			S23MSemanticDomains.minCardinality_1,
+			S23MSemanticDomains.maxCardinality_1,
+			S23MSemanticDomains.isNavigable_TRUE,
+			S23MSemanticDomains.isContainer_FALSE
+	);
+
+	public static final Set agent_to_nativeLanguage = Instantiation.arrow(coreGraphs.edge,
+			CellPlatformDomain.agent_to_nativeLanguage,
+			agent,
+			agent,
+			S23MSemanticDomains.minCardinality_0,
+			S23MSemanticDomains.maxCardinality_n,
+			S23MSemanticDomains.isNavigable_FALSE,
+			S23MSemanticDomains.isContainer_FALSE,
+			CellPlatformDomain.nativeLanguage,
+			CellEngineering.language,
+			S23MSemanticDomains.minCardinality_1,
+			S23MSemanticDomains.maxCardinality_1,
+			S23MSemanticDomains.isNavigable_TRUE,
+			S23MSemanticDomains.isContainer_FALSE
+	);
+
+	public static Set instantiateFeature() {
+
+		return CellEngineering.agency;
+	}
+}
---
+++ org.s23m.cell.platform/src/main/java/org/s23m/cell/platform/models/Licensing.java
@@ -0,0 +1,116 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is S23M.
+ *
+ * The Initial Developer of the Original Code is
+ * SoftMetaWare Ltd. (SoftMetaWare).
+ * Portions created by the Initial Developer are
+ * Copyright (C) 2011 SoftMetaWare Ltd.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ * Jorn Bettin
+ * ***** END LICENSE BLOCK ***** */
+package org.s23m.cell.platform.models;
+
+import static org.s23m.cell.S23MKernel.coreGraphs;
+import static org.s23m.cell.S23MKernel.coreSets;
+
+import org.s23m.cell.Set;
+import org.s23m.cell.api.Instantiation;
+import org.s23m.cell.api.models.Root;
+
+
+public class Licensing {
+
+	private static final Set v6 = Instantiation.arrow(coreGraphs.visibility, CellEngineering.licensing, CellEngineering.timeConsciousness);
+
+	public static final Set license = CellEngineering.licensing.addConcrete(TimeConsciousness.timeConsciousVertex, CellPlatformDomain.license);
+	public static final Set legalEntity = CellEngineering.licensing.addConcrete(TimeConsciousness.timeConsciousVertex, CellPlatformDomain.legalEntity);
+	public static final Set usageLicense = CellEngineering.licensing.addConcrete(TimeConsciousness.timeConsciousVertex, CellPlatformDomain.usageLicense);;
+
+	private static final Set s1 = Instantiation.arrow(coreGraphs.superSetReference, license, TimeConsciousness.timeConsciousVertex);
+	private static final Set s2 = Instantiation.arrow(coreGraphs.superSetReference, usageLicense, TimeConsciousness.timeConsciousVertex);
+	private static final Set s3 = Instantiation.arrow(coreGraphs.superSetReference, legalEntity, TimeConsciousness.timeConsciousVertex);
+	private static final Set v1 = Instantiation.arrow(coreGraphs.visibility, Root.cellengineering, CellEngineering.licensing);
+	public static final Set legalEntity_to_usageLicense = Instantiation.arrow(TimeConsciousness.timeConsciousEdge,
+			CellPlatformDomain.legalEntity_to_usageLicense,
+			CellPlatformDomain.user,
+			legalEntity,
+			coreSets.minCardinality_0,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_FALSE,
+			coreSets.isContainer_FALSE,
+			CellPlatformDomain.usageLicense,
+			usageLicense,
+			coreSets.minCardinality_1,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_TRUE,
+			coreSets.isContainer_FALSE
+	);
+	public static final Set usageLicense_to_license = Instantiation.arrow(TimeConsciousness.timeConsciousEdge,
+			CellPlatformDomain.usageLicence_to_license,
+			CellPlatformDomain.usageLicense,
+			usageLicense,
+			coreSets.minCardinality_0,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_FALSE,
+			coreSets.isContainer_FALSE,
+			license,
+			license,
+			coreSets.minCardinality_1,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_TRUE,
+			coreSets.isContainer_FALSE
+	);
+
+	public static final Set usageLicence_to_vertex = Instantiation.arrow(TimeConsciousness.timeConsciousEdge,
+			CellPlatformDomain.usageLicence_to_vertex,
+			usageLicense,
+			usageLicense,
+			coreSets.minCardinality_0,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_FALSE,
+			coreSets.isContainer_FALSE,
+			CellPlatformDomain.licencedProduct,
+			coreGraphs.vertex,
+			coreSets.minCardinality_1,
+			coreSets.maxCardinality_1,
+			coreSets.isNavigable_TRUE,
+			coreSets.isContainer_FALSE
+	);
+
+	public static final Set usageLicence_to_issuer = Instantiation.arrow(TimeConsciousness.timeConsciousEdge,
+			CellPlatformDomain.usageLicence_to_issuer,
+			usageLicense,
+			usageLicense,
+			coreSets.minCardinality_0,
+			coreSets.maxCardinality_n,
+			coreSets.isNavigable_FALSE,
+			coreSets.isContainer_FALSE,
+			CellPlatformDomain.issuer,
+			legalEntity,
+			coreSets.minCardinality_1,
+			coreSets.maxCardinality_1,
+			coreSets.isNavigable_TRUE,
+			coreSets.isContainer_FALSE
+	);
+
+
+
+	public static Set instantiateFeature() {
+
+		return CellEngineering.licensing;
+	}
+}
