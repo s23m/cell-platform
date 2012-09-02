52364d4eb0c998a205c61c0048e2be4193e8d5e2
ashewring <a.shewring@gmail.com>
2012-09-02 14:46:49 +0200

    Formatting

--- org.s23m.cell.platform/src/main/java/org/s23m/cell/platform/formulaProcessors/FormulaEvaluator.java
+++ org.s23m.cell.platform/src/main/java/org/s23m/cell/platform/formulaProcessors/FormulaEvaluator.java
@@ -4,15 +4,14 @@ import org.s23m.cell.Set;
 import org.s23m.cell.platform.api.models.CellPlatformAgent;
 import org.s23m.cell.platform.api.models.CellPlatformDomain;
 
+public class FormulaEvaluator {
 
-public  class FormulaEvaluator {
-
-	protected   Set subFormula;
-	protected   Set terms;
-	protected   Set variables;
-	protected   Set constants;
-	protected   Set values;
-	protected   Arity arity;
+	protected final Set subFormula;
+	protected final Set terms;
+	protected final Set variables;
+	protected final Set constants;
+	protected final Set values;
+	protected final Arity arity;
 
 	protected enum Arity {
 	    ZERO, UNARY, BINARY, NARY
@@ -24,18 +23,20 @@ public  class FormulaEvaluator {
 		this.variables = variables;
 		this.constants = constants;
 		this.values = values;
-		if (terms.size() == 1) {arity = Arity.UNARY;}
-		else {
-			if (terms.size() == 2) {arity = Arity.BINARY;}
-			else {
-				if (terms.size() > 2) {arity = Arity.NARY;}
-				else {arity = Arity.ZERO;}
-			}
+		final int numberOfTerms = terms.size();
+		if (numberOfTerms == 1) {
+			arity = Arity.UNARY;
+		} else if (numberOfTerms == 2) {
+			arity = Arity.BINARY;
+		} else if (numberOfTerms > 2) {
+			arity = Arity.NARY;
+		} else {
+			arity = Arity.ZERO;
 		}
 	}
 
 	public static FormulaEvaluator create(final Set subFormula, final Set terms, final Set variables, final Set constants, final Set values) {
-		 return  new FormulaEvaluator(subFormula, terms, variables, constants, values);
+		return new FormulaEvaluator(subFormula, terms, variables, constants, values);
 	}
 
 	public Set evaluate() {
@@ -46,17 +47,16 @@ public  class FormulaEvaluator {
 	}
 
 	protected Set generateSemanticIdentityForEvaluationResult() {
-
-	return org.s23m.cell.platform.api.Instantiation.addDisjunctSemanticIdentitySet(
-			CellPlatformDomain.evaluationOf.identity().name()
-			+ CellPlatformDomain.openSquareBracket.identity().name()
-			+ this.subFormula.identity().name()
-			+ CellPlatformDomain.closeSquareBracket.identity().name()
-			,
-			CellPlatformDomain.evaluationOf.identity().name()
-			+ CellPlatformDomain.openSquareBracket.identity().name()
-			+ this.subFormula.identity().name()
-			+ CellPlatformDomain.closeSquareBracket.identity().name()
-			, org.s23m.cell.platform.api.Instantiation.toSemanticDomain(CellPlatformAgent.development));
+		return org.s23m.cell.platform.api.Instantiation.addDisjunctSemanticIdentitySet(
+				CellPlatformDomain.evaluationOf.identity().name()
+				+ CellPlatformDomain.openSquareBracket.identity().name()
+				+ this.subFormula.identity().name()
+				+ CellPlatformDomain.closeSquareBracket.identity().name()
+				,
+				CellPlatformDomain.evaluationOf.identity().name()
+				+ CellPlatformDomain.openSquareBracket.identity().name()
+				+ this.subFormula.identity().name()
+				+ CellPlatformDomain.closeSquareBracket.identity().name()
+				, org.s23m.cell.platform.api.Instantiation.toSemanticDomain(CellPlatformAgent.development));
 	}
 }
