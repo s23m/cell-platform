0c8ed3e3a34893d644eeeefe7f026e17ec421496
Andrew Shewring <andrew.shewring@s23m.com>
2016-06-04 11:19:47 +1200

    Added first cut of Agent table

--- org.s23m.cell.persistence/src/main/resources/standard_ddl.sql
+++ org.s23m.cell.persistence/src/main/resources/standard_ddl.sql
@@ -1,5 +1,26 @@
 -- DDL which should execute correctly against PostgreSQL, MySQL, H2
 
+-- TODO: email length
+-- TODO: password length (scrypt)
+-- TODO: mobile length
+
+CREATE TABLE IF NOT EXISTS Agent (
+  urr varchar(36) NOT NULL,
+  uuid varchar(36) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  mobile varchar(30) NOT NULL,
+  firstName varchar(50) NOT NULL,
+  lastName varchar(50) NOT NULL,
+  alias varchar(50) NOT NULL,
+  PRIMARY KEY (urr),
+  CONSTRAINT uc_uuid_urr UNIQUE (uuid,urr),
+  CONSTRAINT fk_urr FOREIGN KEY (urr) REFERENCES identity (uuid),
+  CONSTRAINT fk_uuid FOREIGN KEY (uuid) REFERENCES identity (uuid)
+);
+
+-- --------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS Identity (
   uuid varchar(36) NOT NULL,
   name varchar(100) NOT NULL,
